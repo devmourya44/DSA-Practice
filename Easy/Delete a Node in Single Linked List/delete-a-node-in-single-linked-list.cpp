9b4b7c6959640010aaf77faa662ad8d2496dd43b
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-13 21:44:27 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete a Node in Single Linked List/delete-a-node-in-single-linked-list.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+           int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+/* Function to get the middle of the linked list*/
+struct Node* deleteNode(struct Node *head,int );
+
+void printList(Node *head)
+{
+   while (head != NULL)
+   {
+      cout << head->data << " ";
+      head = head->next;
+   }
+   cout << "\n";
+}
+
+/* Driver program to test above function*/
+int main()
+{
+  int T, i, n, l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+	int kk;
+	cin>>kk;
+    head = deleteNode(head,kk);
+    printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+/*You are required to complete below method*/
+Node* deleteNode(Node *head,int x)
+{
+    Node* temp = head;
+    Node* prev = NULL;
+    int cnt = 1;
+
+    // Find the node to be deleted
+    while (cnt < x && temp != NULL)
+    {
+        prev = temp;
+        temp = temp->next;
+        cnt++; // Don't forget to increment cnt
+    }
+
+    // If the node was found
+    if (temp != NULL)
+    {
+        // Update the pointers to remove the node
+        if (prev != NULL)
+            prev->next = temp->next;
+        else
+            head = temp->next;
+
+        temp->next = NULL; // Disconnect the node from the list
+        delete temp;       // Free the memory of the deleted node
+    }
+
+    return head;
+
+}
