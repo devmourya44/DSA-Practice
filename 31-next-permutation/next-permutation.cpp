8d4b569bc1529115db6194c8f9e2efd44c0e5952
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-14 09:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (21.63%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // Write your code here.
+
+    next_permutation(nums.begin(),nums.end());
+
+    
+
+    }
+    
+};
