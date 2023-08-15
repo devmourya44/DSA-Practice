248e4f103c4972058fe341cfba34993cd98a54be
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-15 09:40:47 +0530

    Time: 4 ms (78.23%) | Memory: 10.2 MB (83.57%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -1,34 +1,44 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode before(0), after(0);
-        ListNode* before_curr = &before;
-        ListNode* after_curr = &after;
+        // Initialize two new dummy nodes for the 'before' and 'after' lists.
+        ListNode* before = new ListNode(0);
+        ListNode* after = new ListNode(0);
         
-        while(head) {
-            if(head->val < x) {
+        // Use two pointers, one each for the 'before' and 'after' lists, 
+        // to help in appending nodes to these lists.
+        ListNode* before_curr = before;
+        ListNode* after_curr = after;
+
+        // Traverse the original list.
+        while (head) {
+            // Check the value of the current node.
+            if (head->val < x) {
+                // If the node's value is less than x, 
+                // append it to the 'before' list.
                 before_curr->next = head;
-                before_curr = head;
+                before_curr = before_curr->next;
             } else {
+                // If the node's value is greater than or equal to x, 
+                // append it to the 'after' list.
                 after_curr->next = head;
-                after_curr = head;
+                after_curr = after_curr->next;
             }
+
+            // Move to the next node in the original list.
             head = head->next;
         }
         
+        // Ensure the last node in the 'after' list points to nullptr.
         after_curr->next = nullptr;
-        before_curr->next = after.next;
         
-        return before.next;
+        // Connect the last node of the 'before' list to the first node 
+        // (after the dummy node) of the 'after' list.
+        before_curr->next = after->next;
+        
+        // Return the merged list starting from the first node 
+        // (after the dummy node) of the 'before' list.
+        return before->next;
     }
 };
