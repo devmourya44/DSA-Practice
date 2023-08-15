22b1726ccb63e7ee5d965be439d9ad34a7424e7d
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-16 01:08:52 +0530

    Time: 8 ms (72.12%) | Memory: 10 MB (45.67%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
+
+    int minSumPathUtil(int i, int j,vector<vector<int>> &matrix,vector<vector<int>> &dp)
+    {
+        if(i==0 && j == 0)
+            return matrix[0][0];
+        if(i<0 || j<0)
+            return 1e9;
+        if(dp[i][j]!=-1) return dp[i][j];
+            
+        int up = matrix[i][j]+minSumPathUtil(i-1,j,matrix,dp);
+        int left = matrix[i][j]+minSumPathUtil(i,j-1,matrix,dp);
+        
+        return dp[i][j] = min(up,left);
+        
+    }
+
+
+
+
     int minPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> prev(m,0);
-        for(int i=0; i<n ; i++){
-            vector<int> temp(m,0);
-            for(int j=0; j<m; j++){
-                if(i==0 && j==0) temp[j] = matrix[i][j];
-                else{
-                    
-                    int up = matrix[i][j];
-                    if(i>0) up += prev[j];
-                    else up += 1e9;
-                    
-                    int left = matrix[i][j];
-                    if(j>0) left+=temp[j-1];
-                    else left += 1e9;
-                    
-                    temp[j] = min(up,left);
-                }
-            }
-            prev=temp;
-        }
-        
-        return prev[m-1];
+        vector<vector<int> > dp(n,vector<int>(m,-1));
+        return minSumPathUtil(n-1,m-1,matrix,dp);
     }
 };
