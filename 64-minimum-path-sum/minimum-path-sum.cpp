66bcfc71b9f01cd06237588e2397db559040680d
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-16 01:09:51 +0530

    Time: 11 ms (51.60%) | Memory: 10.2 MB (15.56%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
 
-    int minSumPathUtil(int i, int j,vector<vector<int>> &matrix,vector<vector<int>> &dp)
-    {
-        if(i==0 && j == 0)
-            return matrix[0][0];
-        if(i<0 || j<0)
-            return 1e9;
-        if(dp[i][j]!=-1) return dp[i][j];
-            
-        int up = matrix[i][j]+minSumPathUtil(i-1,j,matrix,dp);
-        int left = matrix[i][j]+minSumPathUtil(i,j-1,matrix,dp);
-        
-        return dp[i][j] = min(up,left);
-        
-    }
-
-
-
-
     int minPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int> > dp(n,vector<int>(m,-1));
-        return minSumPathUtil(n-1,m-1,matrix,dp);
+        vector<vector<int> > dp(n,vector<int>(m,0));
+        for(int i=0; i<n ; i++){
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0) dp[i][j] = matrix[i][j];
+                else{
+                    
+                    int up = matrix[i][j];
+                    if(i>0) up += dp[i-1][j];
+                    else up += 1e9;
+                    
+                    int left = matrix[i][j];
+                    if(j>0) left+=dp[i][j-1];
+                    else left += 1e9;
+                    
+                    dp[i][j] = min(up,left);
+                }
+            }
+        }
+    
+        return dp[n-1][m-1];
     }
 };
