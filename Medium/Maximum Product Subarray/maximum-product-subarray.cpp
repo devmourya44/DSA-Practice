77af8f6c84365fc99daa9a025d4def00c447f7ab
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-16 18:03:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Product Subarray/maximum-product-subarray.cpp
+++ Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -9,32 +9,17 @@ class Solution{
 public:
 
 	// Function to find maximum product subarray
-	long long maxProduct(vector<int> arr, int n) {
-
-	    long long maxL = LLONG_MIN;
-	    long long maxR = LLONG_MIN;
-	    long long p = 1;
-	    
-	    for (int i = 0; i < n; i++) {
-	        p *= arr[i];
-	        maxL = max(maxL, p);
-	        if (p == 0) {
-	            p = 1;;
-	        }
-	    }
-	    
-	    p = 1;
-	    
-	    for (int i = n-1; i >= 0; i--) {
-	        p *= arr[i];
-	        maxR = max(maxR, p);
-	        if (p == 0) {
-	            p = 1;
-	        }
-	    }
-	    
-	    return max(maxL,maxR);
-	    
+	long long maxProduct(vector<int>& arr, int n) {
+	    long long pre = 1, suff = 1;
+        long long ans = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            if (pre == 0) pre = 1;
+            if (suff == 0) suff = 1;
+            pre *= arr[i];
+            suff *= arr[n - i - 1];
+            ans = max(ans, max(pre, suff));
+        }
+        return ans;
 	}
 };
 
