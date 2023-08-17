f5c6e6b6e29e85edf48df9dbe148d64594d5d3d2
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-17 21:11:53 +0530

    Updated Solution - GfG to GitHub

--- Medium/Coin Change/coin-change.cpp
+++ Medium/Coin Change/coin-change.cpp
@@ -3,38 +3,23 @@
 using namespace std;
 
 // } Driver Code Ends
+#define ll long long
 class Solution {
   public:
+    
     long long int count(int coins[], int N, int sum) {
-
-       vector<long long int> prev(sum + 1, 0);
-        fill(prev.begin(), prev.end(), 0);
         
-        for (int i = 0; i <= sum; i++) {
-            if (i % coins[0] == 0) {
-                prev[i] = 1;
+        vector<long long>dp(sum+1,0);
+        dp[0]=1;
+        for(int i=0;i<N;i++)
+        {
+            for(int j=coins[i];j<=sum;j++)
+            {
+                dp[j]+=dp[j-coins[i]];
             }
-        }
-        
-        for (int i = 1; i < N; i++) {
-            vector<long long int> curr(sum + 1, 0);
-            fill(curr.begin(), curr.end(), 0);
             
-            for (int j = 0; j <= sum; j++) {
-                long long int a, b;
-                a = b = 0;
-                
-                a = prev[j];
-                if (j >= coins[i]) {
-                    b = curr[j - coins[i]];
-                }
-                
-                curr[j] = a + b;
-            }
-            prev = curr;
         }
-        
-        return prev[sum];
+        return dp[sum];
         
     }
 };
