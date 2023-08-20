3c34591736d8b2650cdda9a90da06c103aeb3396
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-20 09:06:05 +0530

    Updated Solution - GfG to GitHub

--- Medium/Coin Change/coin-change.cpp
+++ Medium/Coin Change/coin-change.cpp
@@ -3,25 +3,31 @@
 using namespace std;
 
 // } Driver Code Ends
-#define ll long long
 class Solution {
   public:
-    
-    long long int count(int coins[], int N, int sum) {
-        
-        vector<long long>dp(sum+1,0);
-        dp[0]=1;
-        for(int i=0;i<N;i++)
-        {
-            for(int j=coins[i];j<=sum;j++)
-            {
-                dp[j]+=dp[j-coins[i]];
+      long long int count(int coins[], int N, int sum) {
+        // Create a dp array of size sum + 1 to store the number of ways
+        // to make each value from 0 to sum using the given coins
+        vector<long long int> dp(sum + 1, 0);
+
+        // There is one way to make sum = 0, i.e., by not using any coins
+        dp[0] = 1;
+
+        // Iterate through the coins array
+        for (int i = 0; i < N; i++) {
+            // Update the dp array for each value from coins[i] to sum
+            // This represents the number of ways to make the current value
+            // using the current coin and previously calculated ways
+            for (int j = coins[i]; j <= sum; j++) {
+                dp[j] += dp[j - coins[i]];
             }
-            
         }
+
+        // The final value at dp[sum] represents the number of ways to make
+        // the given sum using the given coins
         return dp[sum];
-        
     }
+
 };
 
 //{ Driver Code Starts.
