f37c31c77131fe29c3ac3268d4a0cdd7c3fd46fe
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-22 07:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (79.56%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    string convertToTitle(int n) {
-        if (n < 27) {
-            return string(1, 'A' + (n - 1) % 26);
+    std::string convertToTitle(int columnNumber) {
+        std::string out;
+        while (columnNumber > 0) {
+            out = char('A' + (columnNumber - 1) % 26) + out;
+            columnNumber = (columnNumber - 1) / 26;
         }
-        
-        string c = "";
-        while (n > 0) {
-            if (n % 26 == 0) {
-                c += 'A' + 25;
-                n -= 1;
-            } else {
-                c += 'A' + n % 26 - 1;
-            }
-            n /= 26;
-        }
-        reverse(c.begin(), c.end());
-        return c;
+        return out;
     }
-};;
+};
