343c6c5e6d66572aedb65e70570f4f91752d7958
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-25 23:56:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Lucky Numbers/lucky-numbers.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User-function template for C++
+
+// Complete the function
+// n: Input n
+// Return True if the given number is a lucky number else return False
+
+class Solution{
+public:
+    bool ans(int n,int c)
+    {
+        if(n<c)
+        return true;
+        if(n%c==0)return false;
+        ans(n-(n/c),c+1);
+    }
+    bool isLucky(int n) {
+         return ans(n,2);
+    }
+};
+
+//{ Driver Code Starts.
+signed main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling isLucky() function
+        if(obj.isLucky(n))
+            cout<<"1\n";//printing "1" if isLucky() returns true
+        else
+            cout<<"0\n";//printing "0" if isLucky() returns false
+    }
+    
+}
+// } Driver Code Ends
