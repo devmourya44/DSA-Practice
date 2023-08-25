3ee3d04496a02a37194c18ec317dbf98576ba53f
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-25 08:31:30 +0530

    Time: 6 ms (55.25%) | Memory: 7.1 MB (58.49%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int m = s1.length(), n = s2.length(), l = s3.length();
-        if (m + n != l) return false;
+        int n1 = s1.size(), n2 = s2.size(), n3 = s3.size();
+        if (n1 + n2 != n3) return false;
         
-        if (m < n) return isInterleave(s2, s1, s3);
-
-        vector<bool> dp(n + 1, false);
-        dp[0] = true;
-
-        for (int j = 1; j <= n; ++j) {
-            dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
-        }
-
-        for (int i = 1; i <= m; ++i) {
-            dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
-            for (int j = 1; j <= n; ++j) {
-                dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        dp[n1][n2] = 1;
+        
+        for (int i = n1; i >= 0; i--) {
+            for (int j = n2; j >= 0; j--) {
+                if (i<n1 && s1[i]==s3[i+j]) 
+                    dp[i][j] = dp[i][j] || dp[i+1][j];
+                if (j<n2 && s2[j]==s3[i+j]) 
+                    dp[i][j] = dp[i][j] || dp[i][j+1];
             }
-        }
-        
-        return dp[n];
+        }  
+        return dp[0][0] == 1;
     }
 };
