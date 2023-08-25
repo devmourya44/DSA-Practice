9a27cf660ffc7ac5dd7f9481ee606bb28967ff90
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-08-25 18:23:13 +0530

    Time: 483 ms (26.23%) | Memory: 283.2 MB (13.93%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,39 +1,39 @@
 class Solution {
-private: 
-    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
-        if(i == j){
-            return dp[i][j] = true;
-        }
-        if(j-i == 1){
-            if(s[i] == s[j]){
-                return dp[i][j] = true;
-            }
-            else{
-                return dp[i][j] = false;
-            }
-        }
-        if(s[i] == s[j] && dp[i+1][j-1] == true){
-            return dp[i][j] = true;
-        } else {
-            return dp[i][j] = false;
-        }
-    }
+    int maxLen = 1;
+    int start = 0;
+    int end = 0;
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        int startIndex = 0; int maxlen = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int g=0; g<n; g++){
-            for(int i=0, j=g; j<n; i++, j++){
-                solve(dp, i, j, s);
-                if(dp[i][j] == true){
-                    if(j-i+1 > maxlen){
-                        startIndex = i;
-                        maxlen = j-i+1;
-                    }
-                }
-            }
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        longestPalindrome(s, 0, s.length()-1, dp);
+        return s.substr(start, end-start+1);
+    }
+
+    bool longestPalindrome(string& s, int i, int j, vector<vector<int>>& dp) {
+        if(i > j) return true;
+
+        if(dp[i][j] != -1) 
+            return (dp[i][j] == 1);
+
+        if(i == j) {
+            dp[i][j] = 1;
+            return true;
         }
-        return s.substr(startIndex, maxlen);
+
+        dp[i][j] = 0;
+        if(s[i] == s[j] && longestPalindrome(s, i+1, j-1, dp)) {
+            int len = j-i+1;
+            if(len > maxLen) {
+                maxLen = len;
+                start = i;
+                end = j;
+            }
+            dp[i][j] = 1;
+        } else if(!longestPalindrome(s, i+1, j, dp)) {
+            longestPalindrome(s, i, j-1, dp);
+        }
+
+        return dp[i][j] == 1;
     }
 };
