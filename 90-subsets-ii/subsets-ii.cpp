bea7d12ab9cfde551effbfc4a347000cd311b84c
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-04 23:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (76.21%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void fun(vector < int > & nums, int index, vector < int > ds, set < vector < int >> & res) {
-        if (index == nums.size()) {
-          sort(ds.begin(), ds.end());
-          res.insert(ds);
-          return;
-        }
-        ds.push_back(nums[index]);
-        fun(nums, index + 1, ds, res);
-        ds.pop_back();
-        fun(nums, index + 1, ds, res);
-    }
-    vector < vector < int >> subsetsWithDup(vector < int > & nums) {
-        vector < vector < int >> ans;
-        set < vector < int >> res;
-        vector < int > ds;
-        fun(nums, 0, ds, res);
-        for (auto it = res.begin(); it != res.end(); it++) {
-          ans.push_back( * it);
-        }
-        return ans;
-    }
+    void findSubsets(int ind, vector < int > & nums, vector < int > & ds, vector < vector < int >> & ans) {
+         ans.push_back(ds);
+         for (int i = ind; i < nums.size(); i++) {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            ds.push_back(nums[i]);
+            findSubsets(i + 1, nums, ds, ans);
+            ds.pop_back();
+         }
+      }
+   public:
+      vector < vector < int >> subsetsWithDup(vector < int > & nums) {
+         vector < vector < int >> ans;
+         vector < int > ds;
+         sort(nums.begin(), nums.end());
+         findSubsets(0, nums, ds, ans);
+         return ans;
+      }
 };
