a9a99cab714aaf2cf429732c029e650ca30b0a52
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-06 20:34:37 +0530

    Time: 304 ms (84.76%) | Memory: 8.1 MB (20.83%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,46 +1,34 @@
 class Solution {
-public:
-    int rows;
-    int cols;
-    int dx[4] = {1,-1,0,0};
-    int dy[4] = {0,0,1,-1};
-
-    bool backtrack(vector<vector<char>>&board , int i , int j , string word, int index)
-    {
-        if(index == word.size())
-            return true;
+private:
+    bool dfs(vector<vector<char>>& board, vector<vector<bool>>& vis, string &word, int i, int j, int start) {
+        int m = board.size(), n = board[0].size();
+        if(start == word.size()) return true;
+        if(i < 0 || i >= m || j < 0 || j >= n || vis[i][j] || board[i][j] != word[start]) return false;
         
-        if(i >= rows or j >= cols or i < 0 or j < 0 or board[i][j] != word[index])
-            return false;
-
-        char t = board[i][j];
-        board[i][j] = '*';
+        vis[i][j] = true;
+        bool a = dfs(board, vis, word, i+1, j, start+1) ||
+                 dfs(board, vis, word, i-1, j, start+1) ||
+                 dfs(board, vis, word, i, j+1, start+1) ||
+                 dfs(board, vis, word, i, j-1, start+1);
         
-        bool x =
-        backtrack(board,i+dx[0],j+dy[0],word,index+1) or
-        backtrack(board,i+dx[1],j+dy[1],word,index+1) or
-        backtrack(board,i+dx[2],j+dy[2],word,index+1) or
-        backtrack(board,i+dx[3],j+dy[3],word,index+1);
-
-        board[i][j] = t;
-
-        return x;
+        vis[i][j] = false; // Reset the visited state for backtracking
+        return a;
     }
 
-    bool exist(vector<vector<char>>& board, string word)
-    {
-        rows = board.size();
-        cols = board[0].size();
-
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<cols;j++)
-            {
-                if(backtrack(board,i,j,word,0))
-                    return true; 
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dfs(board, vis, word, i, j, 0))
+                    return true;
             }
         }
+        
         return false;
     }
- 
 };
