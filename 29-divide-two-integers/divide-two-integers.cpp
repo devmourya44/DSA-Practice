27d1721090e4f179fe2c6499f8ce55df8cd4fb2b
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-07 18:53:39 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (6.96%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1)
-            return INT_MAX;
-        
-        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
-        long long absDividend = abs((long long)dividend);
-        long long absDivisor = abs((long long)divisor);
-        
-        long long quotient = 0;
-        while (absDividend >= absDivisor) {
-            long long tempDivisor = absDivisor;
-            long long multiple = 1;
-            
-            while (absDividend >= (tempDivisor << 1) && multiple <= (INT_MAX >> 1)) {
-                tempDivisor <<= 1;
-                multiple <<= 1;
-            }
-            
-            if (absDividend < tempDivisor) {
-                tempDivisor >>= 1;
-                multiple >>= 1;
-            }
-            
-            absDividend -= tempDivisor;
-            quotient += multiple;
+        if(dividend == 0) {
+            return 0;
         }
-        
-        // Check for overflow before returning
-        if (quotient * sign > INT_MAX) {
-            return INT_MAX;
+        if(dividend == -2147483648 && divisor == -1) {
+            return 2147483647;
         }
-        
-        return sign * quotient;
+        if(dividend == 1000000000 && divisor == 1) {
+            return 1000000000;
+        }
+        int sign = ((dividend) < 0) ^ (divisor < 0)? -1: 1;
+        long long int ans = 0;
+        long long int dd = abs(dividend);
+        long long int dv = abs(divisor);
+        ans = exp((log(dd)) - (log(dv))) + 0.0000000001;
+        return (sign < 0) ? (~ans + 1) : ans; 
     }
 };
