0c46f10bcf6686319fd356da44ebd879031a59f1
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-08 18:06:46 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Find all possible palindromic partitions of a String/find-all-possible-palindromic-partitions-of-a-string.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<string>> ans;
+    // check weather it is palindrom
+    bool pali(string a){
+        int n = a.size();
+        for(int i=0;i<(n/2);i++){
+            if(a[i]!=a[n-i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // check the every possibility
+    
+    void find(string s,int i,vector<string> v)
+    {
+        if(i>=s.size()){
+            ans.push_back(v);
+            return ;
+        }
+
+        string a = "";
+        for(int k=i;k<s.size();k++){
+            a+=s[k];
+            if(pali(a)){
+                v.push_back(a);
+                find(s,k+1,v);
+                v.pop_back();
+            }
+        }
+        return ;
+    }
+  // given function
+    vector<vector<string>> allPalindromicPerms(string S) {
+        // code here
+        find(S,0,{});
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        
+        cin>>S;
+
+        Solution ob;
+        vector<vector<string>> ptr = ob.allPalindromicPerms(S);
+        
+        for(int i=0; i<ptr.size(); i++)
+        {
+            for(int j=0; j<ptr[i].size(); j++)
+            {
+                cout<<ptr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
