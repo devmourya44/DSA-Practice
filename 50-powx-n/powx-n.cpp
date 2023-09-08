fbe787408a3aefb1af188ce82cb958ead96ffcfc
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-08 10:31:27 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (15.06%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = 1.0;
-        long long nn = n;
-        if (nn < 0) nn = -1 * nn;
-        while (nn) {
-            if (nn % 2) {
-            ans = ans * x;
-            nn = nn - 1;
-            } else {
-            x = x * x;
-            nn = nn / 2;
-            }
+        // base case
+        if(n==0)
+        return 1;
+        if(n==1)
+        return x;
+         if (n < 0)
+       //if n is negative
+          return (1 / myPow(x, -(n+1)) / x);
+        // recursive relation
+        double ans = myPow(x,n/2);
+        // if n is even
+        if(n%2==0){
+            return ans*ans;
         }
-        if (n < 0) ans = (double)(1.0) / (double)(ans);
-        return ans;
+      //if n is odd
+        else{
+            return x*ans*ans;
+        }
     }
 };
