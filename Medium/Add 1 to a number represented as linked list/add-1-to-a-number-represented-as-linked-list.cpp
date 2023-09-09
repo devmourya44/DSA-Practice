17486bc0bc3efb6bc06d7b9e31fbcda117e7d09b
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-09 15:58:56 +0530

    Updated Solution - GfG to GitHub

--- Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.cpp
+++ Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.cpp
@@ -46,40 +46,75 @@ struct Node
 class Solution
 {
     public:
-    Node* reverse(Node* head){
-        if(head==NULL||head->next==NULL){
-            return head;
-        }
-        Node* curr=head;
-        Node* prev=NULL;
-        Node* forw=NULL;
-        while(curr!=NULL){
-            forw=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forw;
+    Node *reverse(Node *head)
+    {
+        Node * prev = NULL;
+        Node * current = head;
+        Node * next;
+        while (current != NULL)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
         }
         return prev;
     }
-    public:
-    Node* addOne(Node *head) 
+     
+    /* Adds one to a linked lists and return the head
+    node of resultant list */
+    Node *addOneUtil(Node *head)
     {
-        Node* rev=reverse(head);
-        Node* temp=rev;
-        int carry=1;
-        while(temp!=NULL){
-            int c=temp->data;
-            temp->data=(c+carry)%10;
-            carry= (c+carry)/10;
-            temp=temp->next;
+        // res is head node of the resultant list
+        Node* res = head;
+        Node *temp;
+     
+        int carry = 1, sum;
+     
+        while (head != NULL) //while both lists exist
+        {
+            // Calculate value of next digit in resultant list.
+            // The next digit is sum of following things
+            // (i) Carry
+            // (ii) Next digit of head list (if there is a
+            // next digit)
+            sum = carry + head->data;
+     
+            // update carry for next calculation
+            carry = (sum >= 10)? 1 : 0;
+     
+            // update sum if it is greater than 10
+            sum = sum % 10;
+     
+            // Create a new node with sum as data
+            head->data = sum;
+     
+            // Move head and second pointers to next nodes
+            temp = head;
+            head = head->next;
         }
-        Node* fi=reverse(rev);
-        if(carry==1){
-            Node* first=new Node(1);
-            first->next=fi;
-            return first;
-        }
-        return fi;
+     
+        // if some carry is still there, add a new node to
+        // result list.
+        if (carry > 0)
+            temp->next = new Node(carry);
+     
+        // return head of the resultant list
+        return res;
+    }
+     
+    // This function mainly uses addOneUtil().
+    Node* addOne(Node *head)
+    {
+        // Reverse linked list
+        head = reverse(head);
+     
+        // Add one from left to right of reversed
+        // list
+        head = addOneUtil(head);
+     
+        // Reverse the modified list
+        return reverse(head);
     }
 };
 
