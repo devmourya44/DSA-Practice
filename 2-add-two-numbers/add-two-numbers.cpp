7d114208c6b8ada7dca50452e118d3bf772c7a1d
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-09 18:12:17 +0530

    Time: 28 ms (54.36%) | Memory: 71.5 MB (37.05%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,28 +1,82 @@
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
+    ListNode* reverse(ListNode* head){
+    if(head==NULL)
+        return NULL;
+    if(head->next==NULL)
+        return head;
+    ListNode* prev=NULL;
+    ListNode* curr=head;
+    ListNode* next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
 
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+    ListNode* addTwoNumbers(ListNode* head1, ListNode* head2) {
+        if(head1==NULL)
+            return head2;
+        if(head2==NULL)
+            return head1;
+      
+        ListNode* anshead=NULL;
+        ListNode* anstail=NULL;
+        int carry=0;
+        while( head1!=NULL&&head2!=NULL){
+            int sum=head1->val+head2->val+carry;
+            int digit=sum%10;
+            ListNode* temp=new ListNode(digit);
+            carry=sum/10;
+            if(anshead==NULL){
+                anshead=temp;
+                anstail=temp;
+            }
+            else{ 
+                anstail->next=temp;
+                anstail=temp;
+                }
+            head1=head1->next;
+            head2=head2->next;
         }
-
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+        while(head1!=NULL){
+            int sum=head1->val+carry;
+            int digit=sum%10;
+            ListNode* temp=new ListNode(digit);
+            carry=sum/10;
+            if(anshead==NULL){
+                anshead=temp;
+                anstail=temp;
+            }
+            else{ 
+                anstail->next=temp;
+                anstail=temp;
+                }
+            head1=head1->next;
+        }
+        while(head2!=NULL){
+            int sum=head2->val+carry;
+            int digit=sum%10;
+            ListNode* temp=new ListNode(digit);
+            carry=sum/10;
+            if(anshead==NULL){
+                anshead=temp;
+                anstail=temp;
+            }
+            else{ 
+                anstail->next=temp;
+                anstail=temp;
+                }
+            head2=head2->next;
+        }
+        while(carry!=0){
+            ListNode* temp=new ListNode(carry);
+            anstail->next=temp;
+            carry=carry/10;
+        }
+        return anshead;
     }
 };
