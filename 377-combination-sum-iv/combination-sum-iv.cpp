733078adfd129209d70d57f436158bf35e4d827b
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-09 08:43:51 +0530

    Time: 4 ms (28.06%) | Memory: 6.6 MB (46.24%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,17 +1,55 @@
+// //RECURSION
+// class Solution {
+// public:
+//     int count(vector<int>&nums, int target){
+//         if(target==0) return 1;
+//         int n=nums.size();
+//         int temp=0;
+//         for(int i=0;i<n;i++){
+//             if(target>=nums[i]) temp+= count(nums, target-nums[i]);
+//         }
+//         return temp;
+//     }
+//     int combinationSum4(vector<int>& nums, int target) {
+//         return count(nums, target);
+//     }
+// };
+
+//memoization
+
 class Solution {
 public:
-    int combinationSum4(std::vector<int>& nums, int target) {
-        std::vector<unsigned int> dp(target + 1, 0);
-        dp[0] = 1;
-        
-        for (int i = 1; i <= target; ++i) {
-            for (int num : nums) {
-                if (i - num >= 0) {
-                    dp[i] += dp[i - num];
-                }
-            }
+    int count(vector<int>&nums, int target, vector<int>&dp){
+        if(target==0) return 1;
+        if(dp[target]!=-1) return dp[target];
+        int n=nums.size();
+        int temp=0;
+        for(int i=0;i<n;i++){
+            if(target>=nums[i]) temp+= count(nums, target-nums[i], dp);
         }
-        
-        return dp[target];
+        return dp[target]= temp;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1, -1);
+        return count(nums, target, dp);
     }
 };
+
+// //tabulation
+
+// class Solution {
+// public:
+//     int combinationSum4(vector<int>& nums, int target) {
+//         vector<int>dp(target+1);
+//         dp[0]=1;
+//         int n=nums.size();
+//         for(int i=1;i<=target;i++){
+//             long long int temp=0;
+//             for(int j=0;j<n;j++){
+//                 if(nums[j]<=i) temp+=dp[i-nums[j]];
+//             }
+//             dp[i]=temp;
+//         }
+//         return dp[target];
+//     }
+// };
