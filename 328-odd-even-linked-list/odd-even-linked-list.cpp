52b19d56f40940b2a3b9c67556a17d984a5be68c
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-09 11:04:43 +0530

    Time: 13 ms (35.81%) | Memory: 10.5 MB (25.11%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL){
-            return head;
-        }   
-        ListNode *odd=head,*even=head->next,*evenHead=even;
-        while(even && even->next){
-            odd->next=even->next;
-            odd=odd->next;
-            even->next=odd->next;
-            even=even->next;
+        if(head==NULL)
+          return NULL;
+        ListNode* o=NULL;      //for traversing and connecting odd index nodes
+        ListNode* es=NULL;     //for linking last odd node to first even index node
+        ListNode* ee=NULL;     //for traversing and connecting even index node
+        ListNode* curr=head;   //for traversing original linked list
+        int count=1;           //for checking even and odd index
+        while(curr!=NULL){
+            if(count&1){
+                if(o==NULL){
+                    o=curr;
+                }
+                else{
+                    o->next=curr;
+                    o=o->next;
+                }
+            }
+            else if(count%2==0){
+                if(es==NULL){
+                    es=curr;
+                    ee=curr;
+                }
+                else{
+                    ee->next=curr;
+                    ee=ee->next;
+                }
+            }
+            count++;
+            curr=curr->next;
         }
-        odd->next=evenHead;
+        o->next=es;
+        if(ee!=NULL)
+           ee->next=NULL;
         return head;
+        
     }
 };
