dbf4fa7e8eccadb542a229a07f1df49bb1eb61e5
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-12 14:17:01 +0530

    Time: 281 ms (6.65%) | Memory: 23.4 MB (98.25%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,7 +4,6 @@ public:
         int n = a.size();
         vector <int> answer(n,-1);
         for (int i = 0; i < n; i++) {
-           
             for (int j = 1; j < n; j++) {
                 if (a[(i + j) % n] > a[i]) {
                     answer[i] = a[(i + j) % n];
@@ -12,7 +11,6 @@ public:
                 }
             }
         }
-        // Return 'answer' as the answer to the problem.
         return answer;
     }
 };
