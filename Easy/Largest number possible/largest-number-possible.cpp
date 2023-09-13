bb0a81cca610c51950273f76d67d2546f7c95a22
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-13 10:08:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Largest number possible/largest-number-possible.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    string findLargest(int N, int s){
+       if (s == 0 && N > 1) return "-1";
+        string ans =  "";
+        int n = 9;
+        while (ans.size() < N){
+                
+                if (s > 9){
+                   ans.push_back('9');
+                    s = s - 9;
+                }
+                else {
+                    if (s > 0) {
+                        ans.push_back(48+s);
+                        s-=s;
+                    }
+                    else ans.push_back('0');
+                }
+        }
+        if (s == 0)
+            return ans;
+        else if (s!=0) return "-1";
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, S;
+        cin>>N>>S;
+        
+        Solution ob;
+        cout<<ob.findLargest(N, S)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
