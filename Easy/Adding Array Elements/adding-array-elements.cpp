3e4a0f088f09f36579f051f9b30d7a0ec1f0b27f
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-09-25 23:00:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Adding Array Elements/adding-array-elements.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int minOperations(int arr[], int n, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        
+        for(int i=0;i<n;i++)
+            pq.push(arr[i]);
+        
+        int sum=0;
+        int cnt=0;
+        
+        while(pq.size()!=1&&pq.top()<k)
+        {
+            int a=pq.top();pq.pop();
+            int b=pq.top();pq.pop();
+            pq.push(a+b);
+            cnt++;
+        }
+        
+        if(pq.top()>=k)
+            return cnt;
+        else 
+            return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        int ans = obj.minOperations(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
