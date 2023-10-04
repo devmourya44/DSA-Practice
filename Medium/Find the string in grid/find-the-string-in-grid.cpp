2d4741a2c8b9eea290625b2f7b3578130170fed3
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-10-04 18:05:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the string in grid/find-the-string-in-grid.cpp
+++ Medium/Find the string in grid/find-the-string-in-grid.cpp
@@ -5,42 +5,62 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
+	// This function searches in all 8-direction from point
+	// (row, col) in grid[][]
+	bool search2D(vector<vector<char>> grid, int row, int col, string word, vector<int>x, vector<int>y)
+	{
+		int R = grid.size();
+		int C = grid[0].size();
+	    // If first character of word doesn't match with
+	    // given starting point in grid.
+	    if (grid[row][col] != word[0])
+	      return false;
+	 
+	    int len = word.size();
+	 
+	    // Search word in all 8 directions starting from (row,col)
+	    for (int dir = 0; dir < 8; dir++)
+	    {
+	        // Initialize starting point for current direction
+	        int k, rd = row + x[dir], cd = col + y[dir];
+	 
+	        // First character is already checked, match remaining
+	        // characters
+	        for (k = 1; k < len; k++)
+	        {
+	            // If out of bound break
+	            if (rd >= R || rd < 0 || cd >= C || cd < 0)
+	                break;
+	 
+	            // If not matched,  break
+	            if (grid[rd][cd] != word[k])
+	                break;
+	 
+	            //  Moving in particular direction
+	            rd += x[dir], cd += y[dir];
+	        }
+	 
+	        // If all character matched, then value of must
+	        // be equal to length of word
+	        if (k == len)
+	            return true;
+	    }
+	    return false;
+	}
 	vector<vector<int>>searchWord(vector<vector<char>>grid, string word){
-	   int n=grid.size();
-        int m=grid[0].size();
-        int p=word.size();
-        set<vector<int>> result_set;
-        vector<int> dx = {-1, 1, 0, 0, -1, 1, -1, 1};
-        vector<int> dy = {0, 0, -1, 1, -1, -1, 1, 1};
-     
-        for (int i = 0; i < n; i++) 
-        {
-            for (int j = 0; j < m; j++) 
-            {
-                if (grid[i][j] == word[0]) 
-                { // If the first character matches
-                    for (int dir = 0; dir < 8; dir++) 
-                    { // Check all 8 directions
-                        int x = i, y = j, k;
-                        for (k = 0; k < p; k++) 
-                        {
-                            if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] != word[k])
-                                break;
-                            x += dx[dir];
-                            y += dy[dir];
-                        }
-                        if (k == p) 
-                        { // If the word is found
-                            vector<int> location = {i, j};
-                            result_set.insert(location);
-                        }
-                    }
-                }
-            }
-        }
-        vector<vector<int>> result(result_set.begin(), result_set.end());
-        
-        return result;
+		int row = grid.size();
+		int col = grid[0].size();
+		vector<int>x = { -1, -1, -1, 0, 0, 1, 1, 1 };
+		vector<int>y = { -1, 0, 1, -1, 1, -1, 0, 1 };
+		vector<vector<int>>ans;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < col; j++){
+				if(search2D(grid, i, j, word, x, y)){
+					ans.push_back({i, j});
+				}
+			}
+		}
+		return ans;
 	}
 };
 
