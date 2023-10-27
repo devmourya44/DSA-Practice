30a9ca2852653ed26b4edae4db782c2ec82437ae
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-10-27 11:20:39 +0530

    Time: 39 ms (62.13%) | Memory: 171.8 MB (19.95%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,39 +1,24 @@
+#include <iostream>
+using namespace std;
+
 class Solution {
-    int maxLen = 1;
-    int start = 0;
-    int end = 0;
 public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        longestPalindrome(s, 0, s.length()-1, dp);
-        return s.substr(start, end-start+1);
+string expandAroundCenter(string s, int left, int right) {
+    while (left >= 0 && right < s.length() && s[left] == s[right]) {
+        left--;
+        right++;
     }
+    return s.substr(left + 1, right - left - 1);
+}
 
-    bool longestPalindrome(string& s, int i, int j, vector<vector<int>>& dp) {
-        if(i > j) return true;
-
-        if(dp[i][j] != -1) 
-            return (dp[i][j] == 1);
-
-        if(i == j) {
-            dp[i][j] = 1;
-            return true;
-        }
-
-        dp[i][j] = 0;
-        if(s[i] == s[j] && longestPalindrome(s, i+1, j-1, dp)) {
-            int len = j-i+1;
-            if(len > maxLen) {
-                maxLen = len;
-                start = i;
-                end = j;
-            }
-            dp[i][j] = 1;
-        } else if(!longestPalindrome(s, i+1, j, dp)) {
-            longestPalindrome(s, i, j-1, dp);
-        }
-
-        return dp[i][j] == 1;
+string longestPalindrome(string s) {
+    string longest = "";
+    for (int i = 0; i < s.length(); i++) {
+        string odd = expandAroundCenter(s, i, i);
+        string even = expandAroundCenter(s, i, i + 1);
+        if (odd.length() > longest.length()) longest = odd;
+        if (even.length() > longest.length()) longest = even;
     }
+    return longest;
+}
 };
