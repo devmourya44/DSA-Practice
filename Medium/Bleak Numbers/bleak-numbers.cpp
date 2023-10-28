0384e6b55dae8b3b31988a29e3788769b95ac662
Devendra Mourya <83852465+devmourya44@users.noreply.github.com>
2023-10-29 00:16:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Bleak Numbers/bleak-numbers.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	int is_bleak(int n)
+	{
+	    int life=31;
+	    int x=n;
+	    while(life-- and x>=0){
+	        if(x+__builtin_popcount(x)==n)return 0;
+	        x--;
+	    }
+	    return 1;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	Solution ob;
+    	int ans = ob.is_bleak(n);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
